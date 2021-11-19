1b48e498d02fe992adb85f7e8c468105a4aa6e24
Neo Hornberger <neo.hornberger@gmx.de>
2021-11-19 13:04:54 +0100

    Added first classes for the update mechanism

---
+++ src/TournamentSystem/Controller/Admin/UpdateController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TournamentSystem\Controller\Admin;
+
+use TournamentSystem\Controller\Controller;
+
+class UpdateController extends Controller {
+	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	protected function get(): int {
+		return parent::get();
+	}
+}
