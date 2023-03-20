8204a053b7983a0d1fa0fc6347e8aa7201d451df
Neo Hornberger <neo.hornberger@gmx.de>
2023-03-20 22:18:57 +0100

    Upgraded module system

--- src/TournamentSystem/Module/Module.php
+++ src/TournamentSystem/Module/Module.php
@@ -4,18 +4,16 @@ namespace TournamentSystem\Module;
 
 use ReflectionClass;
 use TournamentSystem\Controller\Controller;
-use TournamentSystem\Database\Database;
+use TournamentSystem\TournamentSystem;
 
 abstract class Module {
-	public static Database $db;
-	
 	/**
 	 * @var array<string, Module>
 	 */
 	private static array $INSTANCES = [];
 	
-	private string $name;
-	private string $path;
+	public readonly string $name;
+	public readonly string $path;
 	
 	protected function __construct($name) {
 		$this->name = $name;
@@ -28,35 +26,37 @@ abstract class Module {
 	public abstract function handle(array $page): ?Controller;
 	
 	public final function install(): void {
-		self::$db->multi_query(file_get_contents($this->path . '/sql/create.sql'));
+		$db = TournamentSystem::instance()->getDatabase();
 		
-		while(self::$db->more_results()) {
-			self::$db->next_result();
+		$db->multi_query(file_get_contents($this->path . '/sql/create.sql'));
+		
+		while($db->more_results()) {
+			$db->next_result();
 		}
 	}
 	
 	public final function uninstall(): void {
-		self::$db->query('SET FOREIGN_KEY_CHECKS = FALSE');
+		$db = TournamentSystem::instance()->getDatabase();
 		
-		self::$db->multi_query(file_get_contents($this->path . '/sql/drop.sql'));
-		while(self::$db->more_results()) {
-			self::$db->next_result();
+		$db->query('SET FOREIGN_KEY_CHECKS = FALSE');
+		
+		$db->multi_query(file_get_contents($this->path . '/sql/drop.sql'));
+		while($db->more_results()) {
+			$db->next_result();
 		}
 		
-		self::$db->query('SET FOREIGN_KEY_CHECKS = TRUE');
+		$db->query('SET FOREIGN_KEY_CHECKS = TRUE');
 	}
 	
 	protected static function set(string $name, mixed $value): void {
 		$_REQUEST[$name] = $value;
 	}
 	
-	public final function getName(): string {
-		return $this->name;
-	}
-	
 	public static function load(string $name): ?Module {
 		if(!array_key_exists($name, self::$INSTANCES)) {
-			$stmt = self::$db->prepare("SELECT module FROM tournament_modules WHERE name=?");
+			$db = TournamentSystem::instance()->getDatabase();
+			
+			$stmt = $db->prepare("SELECT module FROM tournament_modules WHERE name=?");
 			$stmt->bind_param("s", $name);
 			$stmt->execute();
 			
